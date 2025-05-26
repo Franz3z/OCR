8135d1d092f1b0d631bb9111e4dbd13339fa09b6
Franz3z <155528506+Franz3z@users.noreply.github.com>
2025-05-26 17:54:22 +0800

    Update ocrapp.py

--- ocrapp.py
+++ ocrapp.py
@@ -5,7 +5,7 @@ import pytesseract
 import base64
 from io import BytesIO
 
-pytesseract.pytesseract.tesseract_cmd = os.getenv('TESSERACT_CMD', 'C:\Program Files\Tesseract-OCR\tesseract.exe
+pytesseract.pytesseract.tesseract_cmd = os.getenv('TESSERACT_CMD', r'C:\Program Files\Tesseract-OCR\tesseract.exe
 ')
 
 app = Flask(__name__)
